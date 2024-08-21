efb743239492e4f7bd06c83d648a26b8ceb59551
Meliodas <31974197+Melioli@users.noreply.github.com>
2024-08-21 07:23:52 +0200

    Last shit

---
+++ Editor/Manager/HoyoToonManager.cs
@@ -0,0 +1,69 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace HoyoToon
+{
+    public class HoyoToonManager
+    {
+        #region Quick Access Buttons
+
+        [MenuItem("Assets/HoyoToon/Full Setup", priority = 0)]
+        private static void FullSetup()
+        {
+            HoyoToonMaterialManager.GenerateMaterialsFromJson();
+            SetupFBX();
+            GameObject selectedObject = HoyoToonSceneManager.AddSelectedObjectToScene();
+            if (selectedObject != null)
+            {
+                HoyoToonMeshManager.GenTangents(selectedObject);
+            }
+
+        }
+
+        [MenuItem("Assets/HoyoToon/Mesh/Setup FBX", priority = 10)]
+        private static void SetupFBX()
+        {
+            HoyoToonMeshManager.SetFBXImportSettings(HoyoToonParseManager.GetAssetSelectionPaths());
+        }
+
+        [MenuItem("GameObject/HoyoToon/Mesh/Generate Tangents", priority = 21)]
+        private static void GenerateTangents()
+        {
+            GameObject selectedObject = Selection.activeGameObject;
+            if (selectedObject == null)
+            {
+                EditorUtility.DisplayDialog("Error", "No Model selected. Please select a Model to generate tangents.", "OK");
+                HoyoToonLogs.WarningDebug("No Model selected. Please select a Model to generate tangents.");
+                return;
+            }
+
+            HoyoToonMeshManager.GenTangents(selectedObject);
+        }
+
+        [MenuItem("HoyoToon/Socials/Twitter", priority = 21)]
+        static void MenuHoyoToonTwitter()
+        {
+            Application.OpenURL("https://www.twitter.com/Meliodas7DL");
+        }
+
+        [MenuItem("HoyoToon/Socials/Discord", priority = 20)]
+        static void MenuHoyoToonDiscord()
+        {
+            Application.OpenURL("https://discord.gg/meliverse");
+        }
+
+        [MenuItem("HoyoToon/Resources/Documentation", priority = 10)]
+        private static void OpenDocumentation()
+        {
+            Application.OpenURL("https://docs.hoyotoon.com");
+        }
+
+        [MenuItem("HoyoToon/Resources/Asset Repo", priority = 11)]
+        private static void OpenAssetRepo()
+        {
+            Application.OpenURL("https://github.com/Melioli/HoyoToon-Assets");
+        }
+
+        #endregion
+    }
+}
